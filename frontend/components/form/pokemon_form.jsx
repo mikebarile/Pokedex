6972114d8cca88c3ceea629048be1fe50d09275d
App Academy Student <student@appacademy.io>
2016-10-27 17:46:27 -0700

    Created form

--- frontend/components/form/pokemon_form.jsx
+++ frontend/components/form/pokemon_form.jsx
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router';
 class PokemonForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {moves: []};
     this.update = this.update.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -13,14 +13,87 @@ class PokemonForm extends React.Component {
     return e => this.setState({[property]: e.target.value});
   }
 
-  handleSubmit() {
+  updateArray(property, num) {
+    return e => {
+      let arr = this.state[property];
+      arr[num] = e.target.value;
+      this.setState({[property]: arr});
+    };
+  }
 
+  handleSubmit() {
+    console.log(this.state);
+    return e => {
+      e.preventDefault();
+      this.props.createPokemon(this.state);
+    };
   }
 
   render() {
 
     return(
-      <div>1</div>
+      <div>
+        <h1>Create a pokemon!</h1>
+
+        <label>Name:
+          <input type="text" onChange={this.update("name")}/>
+        </label>
+        <br/>
+
+        <label>Attack:
+          <input type="text" onChange={this.update("attack")}/>
+        </label>
+        <br/>
+
+        <label>Defense:
+          <input type="text" onChange={this.update("defense")}/>
+        </label>
+        <br/>
+
+        <label>First move:
+          <input type="text" onChange={this.updateArray("moves", 0)}/>
+        </label>
+        <br/>
+
+        <label>Second move:
+          <input type="text" onChange={this.updateArray("moves", 1)}/>
+        </label>
+        <br/>
+
+        <label>Third move:
+          <input type="text" onChange={this.updateArray("moves", 2)}/>
+        </label>
+        <br/>
+
+        <label>Image URL:
+          <input type="text" onChange={this.update("image_url")}/>
+        </label>
+        <br/>
+
+        <label>Type:
+          <select onChange={this.update("poke_type")}>
+            <option value="fire">fire</option>
+            <option value="electric">electric</option>
+            <option value="normal">normal</option>
+            <option value="ghost">ghost</option>
+            <option value="psychic">psychic</option>
+            <option value="water">water</option>
+            <option value="bug">bug</option>
+            <option value="dragon">dragon</option>
+            <option value="grass">grass</option>
+            <option value="fighting">fighting</option>
+            <option value="ice">ice</option>
+            <option value="flying">flying</option>
+            <option value="poison">poison</option>
+            <option value="ground">ground</option>
+            <option value="rock">rock</option>
+            <option value="steel">steel</option>
+          </select>
+        </label>
+        <br/>
+
+        <button onClick={this.handleSubmit}>Submit!</button>
+      </div>
     );
   }
 }
